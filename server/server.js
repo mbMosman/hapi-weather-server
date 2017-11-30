0072e5bb2c6abd781d1117541b3d4cf120233eb4
mbmosman <mbMosman@users.noreply.github.com>
2017-11-29 21:20:17 -0600

    fix server host/port info for heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 const Weather = require('../modules/weather');
 
 const Hapi = require('hapi');
-const server = new Hapi.Server({ host: 'localhost', port: 3000});
+const server = new Hapi.Server({ host: 'mighty-brushlands-35956.herokuapp.com/', port: 8080});
 
 server.start()
   .then( () => { console.log(`Listening on ${server.info.uri}`)} )
